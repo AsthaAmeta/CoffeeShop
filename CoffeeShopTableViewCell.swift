38b1e4ce0dbab984ea027a5501b3e203f1b669da
AsthaAmeta <ameta.astha@gmail.com>
2022-04-29 16:44:04 +0530

    Add files via upload

---
+++ CoffeeShopTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CoffeeShopTableViewCell.swift
+//  SNInterview
+//
+//  Created by Ameta, Astha on 28/04/22.
+//  Copyright © 2022 ServiceNow. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeShopTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
